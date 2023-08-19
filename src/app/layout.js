ba0340efc446ba189ac97d067919e992ce541b7c
JSman225 <leonardikarl@gmail.com>
2023-08-19 16:26:51 -0400

    more stuff

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,46 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Head from "next/head";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Swooshly Next.js',
-  description: 'Swooshly (made with Next.js)',
-}
+  title: "Swooshly Next.js",
+  description: "Swooshly (made with Next.js)",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="keywords"
+          content="Swooshly, swooshly, confidence, admirers"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="2 days" />
+        <meta name="author" content="Deltasoft" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#111111" />
         <meta name="msapplication-TileColor" content="#111111" />
@@ -23,5 +48,5 @@ export default function RootLayout({ children }) {
       </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
